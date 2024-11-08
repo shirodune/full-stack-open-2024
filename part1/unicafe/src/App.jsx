aaccc8716bbbfcb4a6e87bb911d2be485629e8fe
shirodune <182082960+shirodune@users.noreply.github.com>
2024-11-08 12:37:41 +0800

    Finish Exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -12,11 +12,27 @@ const GiveFeedback = ({good, setGood, neutral, setNeutral, bad, setBad}) => {
 }
 
 const Statistics = ({good, neutral, bad}) => {
+  const total = good + neutral + bad
+  const average = (good*1 + neutral*0 + bad*(-1)) / total
+  const positive = (good) / total * 100
+  if(positive) {
+    return (
+      <div>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>average {average}</p>
+        <p>positive {positive}%</p>
+      </div>
+    )
+  }
   return (
     <div>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>average {average}</p>
+      <p>positive none</p>
     </div>
   )
 }
