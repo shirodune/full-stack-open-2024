482a72f09d226abe3849aa54acc44bf604c3797f
shirodune <182082960+shirodune@users.noreply.github.com>
2024-11-11 11:43:03 +0800

    Finish Exercise 2.4-2.5

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -2,7 +2,7 @@ import Header from './Header'
 import Content from './Content'
 import Total from './Total'
 
-const Course = ({course}) => {
+const Course = ({key, course}) => {
   return (
     <div>
       <Header courseName={course.name} />
