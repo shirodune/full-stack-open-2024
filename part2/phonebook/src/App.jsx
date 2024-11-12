8c3c747cf148f47f5d39438d16608e5eb7f8b27b
shirodune <182082960+shirodune@users.noreply.github.com>
2024-11-12 11:02:09 +0800

    Finish Exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,16 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' , phone: '040-1234567'}
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
+  const [filterName, setFilterName] = useState('')
+
+  const personsToShow = persons.filter(person => person.name.includes(filterName))
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
@@ -17,13 +23,18 @@ const App = () => {
     setNewPhone(event.target.value)
   }
 
+  const handleFilterNameChange = (event) => {
+    console.log(event.target.value)
+    setFilterName(event.target.value)
+  }
+
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
       name: newName,
       phone: newPhone
     }
-    if(persons.find((person) => JSON.stringify(person) === JSON.stringify(personObject))) {
+    if(persons.find( person => JSON.stringify(person) === JSON.stringify(personObject))) {
       alert(`${newName} is already added to phonebook`)
       return
     }
@@ -35,6 +46,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filterName} onChange={handleFilterNameChange}/>    
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
@@ -47,7 +62,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => <div key={person.name}> {person.name} {person.phone} </div>
+      {personsToShow.map((person) => <div key={person.name}> {person.name} {person.phone} </div>
       )}
     </div>
   )
