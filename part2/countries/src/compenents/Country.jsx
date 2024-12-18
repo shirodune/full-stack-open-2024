bea7ac9d77c958b45491ff34bfbc2445372a8d30
shirodune <182082960+shirodune@users.noreply.github.com>
2024-12-18 18:24:44 +0800

    Finish Exercise 2.20

--- part2/countries/src/compenents/Country.jsx
+++ part2/countries/src/compenents/Country.jsx
@@ -1,7 +1,19 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+const api_key = import.meta.env.VITE_SOME_KEY
+
 const Country = ({country}) => {
   console.log(country);
-  const languages = Object.entries(country.languages)
-  console.log(languages);
+  const [weather, setWeather] = useState(null)
+  useEffect(()=> {
+    const location = 
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${api_key}`)
+      .then(response => {
+        setWeather(response.data)
+        console.log(response.data);
+      })
+  }, [])
   
   return <div>
     <h2>{country.name.common}</h2>
@@ -11,12 +23,17 @@ const Country = ({country}) => {
     <div>
       <strong>languages</strong>
       <ul> {
-        languages.map((language) =>
+        Object.entries(country.languages).map((language) =>
             <li key={language[0]}>{language[1]}</li>
         )}        
       </ul>
     <div><img src={country.flags.png} alt={country.flags.alt} /></div>
     </div>
+    {weather && <div><h2>Weather in {country.capital}</h2>
+    <div>temperature {(weather.main.temp-273.15).toFixed(2)} Celcius</div>
+    <div></div>
+    <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}/>
+    <div>wind {weather.wind.speed} m/s</div></div>}
   </div>
 }
 
