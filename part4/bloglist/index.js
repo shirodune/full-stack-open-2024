4333e39af3dc40219ff1048e7427fa3dd4ad7c88
shirodune <182082960+shirodune@users.noreply.github.com>
2025-02-10 21:24:27 +0800

    Finish Exercise 4.2

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,51 +1,6 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const config = require('./utils/config')
+const app = require('./app')
 
-const Blog  = require('./models/Bloglist')
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endponint' })
-}
-
-app.use(unknownEndpoint)
-
-const errorHandler = (error, request, response, next) => {
-  console.log(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
-
-  next(error)
-}
-app.use(errorHandler)
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
