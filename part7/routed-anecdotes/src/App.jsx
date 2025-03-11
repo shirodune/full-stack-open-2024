6077143392b43c3e6cb0f043fa9742d483d76733
shirodune <182082960+shirodune@users.noreply.github.com>
2025-03-11 21:27:32 +0800

    Finish Exercise 7.4-7.5

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Routes, Route, Link, useMatch, useNavigate } from 'react-router'
+import { useField } from './hooks'
 
 const Menu = () => {
   const padding = {
@@ -56,18 +57,18 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content= useField('text')
+  const author = useField('text')
+  const info = useField('text')
   let navigate = useNavigate()
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     props.setNotification(`a new anecdote ${content} created!`)
@@ -75,23 +76,31 @@ const CreateNew = (props) => {
     setTimeout(() => props.setNotification(''), 5000)
   }
 
+  const handleReset = (e) => {
+    e.preventDefault()
+    content.setValue('')
+    author.setValue('')
+    info.setValue('')
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} onReset={handleReset}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info} />
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
+        <button type="reset">reset</button>
       </form>
     </div>
   )
