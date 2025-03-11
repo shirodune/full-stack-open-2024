60ce9eaea434f09fb26d5d755c5607e6af68242c
shirodune <182082960+shirodune@users.noreply.github.com>
2025-03-11 15:36:16 +0800

    Finish Exercise 7.1-7.3

---
+++ part7/routed-anecdotes/src/main.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { BrowserRouter } from 'react-router'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
